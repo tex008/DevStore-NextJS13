0e586a0574474fd03edfaec0d27e83a2a78b6e50
tex008 <texera009@gmail.com>
2023-12-29 00:26:26 -0300

    feat: create route home for the store

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/header";
+import { ReactNode } from "react";
+
+interface StoreLayoutProps {
+  children: ReactNode;
+}
+
+export default function StoreLayout(props: StoreLayoutProps) {
+  return (
+    <div>
+      <Header />
+      {props.children}
+    </div>
+  );
+}
