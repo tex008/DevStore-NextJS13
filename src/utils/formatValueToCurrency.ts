e154437cbfc834b726189c7bfe476ea867817213
tex008 <texera009@gmail.com>
2024-01-03 09:24:56 -0300

    feat: create util to transform a number into currency value

---
+++ src/utils/formatValueToCurrency.ts
@@ -0,0 +1,8 @@
+export function formatValueToCurrency(value: number) {
+  return value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
