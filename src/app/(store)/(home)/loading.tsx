bd1d93f36826c2bfb5ab03d4b691b7c32ba146df
tex008 <texera009@gmail.com>
2024-01-03 14:04:11 -0300

    feat: create loading skeleton for the home page

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { LoadingSkeletion } from '@/components/loadingSkeleton'
+import React from 'react'
 
 export default function Loading() {
-  return <div>loading</div>;
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <LoadingSkeletion className="col-span-6 row-span-6 h-[920px]" />
+      <LoadingSkeletion className="col-span-3 row-span-3" />
+      <LoadingSkeletion className="col-span-3 row-span-3" />
+    </div>
+  )
 }
