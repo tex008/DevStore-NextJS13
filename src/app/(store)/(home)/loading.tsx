5d421a340a947929e7da0821c2bc8bd936b750b2
tex008 <texera009@gmail.com>
2024-01-05 16:41:42 -0300

    fix: import name

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,12 +1,12 @@
-import { LoadingSkeletion } from '@/components/loadingSkeleton'
+import { LoadingSkeleton } from '@/components/loadingSkeleton'
 import React from 'react'
 
 export default function Loading() {
   return (
     <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
-      <LoadingSkeletion className="col-span-6 row-span-6 h-[920px]" />
-      <LoadingSkeletion className="col-span-3 row-span-3" />
-      <LoadingSkeletion className="col-span-3 row-span-3" />
+      <LoadingSkeleton className="col-span-6 row-span-6 h-[920px]" />
+      <LoadingSkeleton className="col-span-3 row-span-3" />
+      <LoadingSkeleton className="col-span-3 row-span-3" />
     </div>
   )
 }
