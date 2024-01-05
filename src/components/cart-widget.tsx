dfe40ad6067691c6d7562afc4b199a1a1ee105b8
tex008 <texera009@gmail.com>
2024-01-05 01:12:28 -0300

    chore: adds comment to explain the amount showed in the cart

--- src/components/cart-widget.tsx
+++ src/components/cart-widget.tsx
@@ -10,8 +10,9 @@ export default function CartWidget() {
   return (
     <div className="flex items-center gap-2">
       <ShoppingBag className="h-4 w-4" />
+      {/* the cart will only show the number of different types of products are in
+      the cart, not necessarily the total amount */}
       <span className="text-sm ">Cart ({items.length})</span>
-
       <div className="w-px h-4 bg-zinc-700" />
     </div>
   )
