b8b93f3ad582e9423ee104a431b12b1197e648fe
tex008 <texera009@gmail.com>
2024-01-05 18:47:17 -0300

    add more tests to the search and product pages

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,23 @@
+describe('Add product to cart', () => {
+  it('should be able to search for products', () => {
+    cy.visit('/')
+
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.location('pathname').should('include', '/search')
+    cy.location('search').should('include', 'q=moletom')
+
+    cy.get('a[href^="/product"').should('exist')
+  })
+
+  it('should be the user redirected to home page when visiting the search page withoout a search query', () => {
+    cy.on('uncaught:exception', () => {
+      // reject the execption for the test
+      return false
+    })
+
+    cy.visit('/search')
+
+    cy.location('pathname').should('equal', '/')
+  })
+})
